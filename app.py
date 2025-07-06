5106ea87891310555745b5ef55c62b7e67ac2811
naqinizam <naqinizam186@gmail.com>
2025-07-07 07:35:48 +0800

    Add gunicorn dependency

--- app.py
+++ app.py
@@ -1,28 +1,12 @@
-from flask import Flask, render_template, send_from_directory
-from flask_cors import CORS
+from flask import Flask, render_template
+from flask_cors import CORS  # Required for iOS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Enable CORS
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/how-it-works')
-def how_it_works():
-    return render_template('how-it-works.html')
-
-@app.route('/ar-viewer')
-def ar_viewer():
     return render_template('ar-viewer.html')
 
-@app.route('/models/<path:filename>')
-def serve_model(filename):
-    return send_from_directory('static/models', filename)
-
-@app.route('/static/<path:filename>')
-def static_files(filename):
-    return send_from_directory('static', filename)
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
