caff30f64d3c948d67fbe8291d75e5790174165e
naqinizam <naqinizam186@gmail.com>
2025-07-07 07:41:38 +0800

    Fix dependency versions

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
-from flask_cors import CORS  # Required for iOS
+from flask import Flask, render_template, send_from_directory
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS
+CORS(app)
 
 @app.route('/')
 def home():
     return render_template('ar-viewer.html')
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
